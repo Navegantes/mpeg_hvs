b75e7dbb9f98805d2a09a23e2bcb17249ce247ba
LuanAGoncalves <eng.luangonsalves@gmail.com>
2015-11-18 18:44:40 -0300

    18/11/2015

--- mpegCodec/codec.py
+++ mpegCodec/codec.py
@@ -114,26 +114,24 @@ class Encoder:
 				va = sqrt(vh**2+vt**2)
 				qhvs = np.zeros((8,8,3), np.float32)
 				g = np.zeros((8,8,3), np.float32)
-				for i in range (8):
-					for j in range (8):
-						v = va
-						ai = (mh*self.oR*0.5*i)/(self.mbr*self.mbc)
-						aj = (mt*self.oC*0.5*j)/(self.mbr*self.mbc)
-						aij = ai + aj
-						if v==0.0:
-							g[i,j] = 0.0
-						else:
+				if va != 0:
+					for i in range (8):
+						for j in range (8):
+							v = va
+							ai = (mh*self.oR*0.5*i)/(self.mbr*self.mbc)
+							aj = (mt*self.oC*0.5*j)/(self.mbr*self.mbc)
+							aij = ai + aj
 							g[i,j] = (6.1+7.3*abs(log(v/3.0))**3.0)*(v*aij**2.0)*exp(-2.0*aij*(v+2.0)/45.9)
 				
-				gmax = np.max(g)
-				for i in range (8):
-					for j in range (8):
-						if gmax == 0.0:
-							qhvs[i,j] = 0.0
-						elif gmax==g[i,j,0]:
-							qhvs[i,j] = 0.0
-						else:
-							qhvs[i,j] = (mh+mt)/(float(self.p)*(1.-(g[i,j]/gmax)))
+					gmax = np.max(g)
+					for i in range (8):
+						for j in range (8):
+							if gmax == 0.0:
+								qhvs[i,j] = 0.0
+							elif gmax==g[i,j,0]:
+								qhvs[i,j] = 0.0
+							else:
+								qhvs[i,j] = (mh+mt)/(float(self.p)*(1.-(g[i,j]/gmax)))
 
 				tables[x][y] = qflat + qhvs
 				
@@ -485,27 +483,24 @@ class Decoder:
 				va = sqrt(vh**2+vt**2)
 				qhvs = np.zeros((8,8,3), np.float32)
 				g = np.zeros((8,8,3), np.float32)
-#				print va,
-				for i in range (8):
-					for j in range (8):
-						v = va
-						ai = (mh*int(self.shape[0])*0.5*i)/(int(self.mbr)*int(self.mbc))
-						aj = (mt*int(self.shape[1]*0.5*j))/(int(self.mbr)*int(self.mbc))
-						aij = ai + aj
-						if v==0.0:
-							g[i,j] = 0.0
-						else:
+				if va != 0:
+					for i in range (8):
+						for j in range (8):
+							v = va
+							ai = (mh*int(self.shape[0])*0.5*i)/(int(self.mbr)*int(self.mbc))
+							aj = (mt*int(self.shape[1]*0.5*j))/(int(self.mbr)*int(self.mbc))
+							aij = ai + aj
 							g[i,j] = (6.1+7.3*abs(log(v/3.0))**3.0)*(v*aij**2.0)*exp(-2.0*aij*(v+2.0)/45.9)
 				
-				gmax = np.max(g)
-				for i in range (8):
-					for j in range (8):
-						if gmax == 0.0:
-							qhvs[i,j] = 0.0
-						elif gmax==g[i,j,0]:
-							qhvs[i,j] = 0.0
-						else:
-							qhvs[i,j] = (mh+mt)/(float(self.p)*(1.-(g[i,j]/gmax)))
+					gmax = np.max(g)
+					for i in range (8):
+						for j in range (8):
+							if gmax == 0.0:
+								qhvs[i,j] = 0.0
+							elif gmax==g[i,j,0]:
+								qhvs[i,j] = 0.0
+							else:
+								qhvs[i,j] = (mh+mt)/(float(self.p)*(1.-(g[i,j]/gmax)))
 
 				tables[x][y] = qflat + qhvs
 				
